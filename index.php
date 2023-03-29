a90f359249c76d8d7e1446402af8c43223093b30
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-03-29 14:04:20 +0200

    Models e products

--- index.php
+++ index.php
@@ -1,15 +1,100 @@
+<?php
+
+include __DIR__ . "/Data/products.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include __DIR__ . "/Template/head.php"; ?>
 </head>
 
 <body>
+    <?php include __DIR__ . "/Template/navbar.php"; ?>
 
+    <div class="container pt-5 mt-5">
+        <h4 class="my-3">
+            Prodotti per i cani
+        </h4>
+        <div class="row row-cols-3 g-3">
+            <?php foreach ($products_dog as $product) : ?>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <i class="<?= $product->getCategory()->icon ?>"></i>
+                                <?= $product->getName() ?>
+                                <small class="text-muted">
+                                    <?= $product->getTextClassname() ?>
+                                </small>
+                            </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                <?php if ($product instanceof ProductBed || $product instanceof ProductFood) : ?>
+                                    <span><?= $product->getFullWeight() ?></span>
+                                <?php endif; ?>
+
+                                <?php if ($product instanceof ProductBed || $product instanceof ProductToy) : ?>
+                                    <span><?= $product->material ?></span>
+                                <?php endif; ?>
+
+                                <?php if ($product instanceof ProductFood) : ?>
+                                    <span><?= $product->type ?></span>
+                                <?php endif; ?>
+                            </h6>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, quasi.
+                            </p>
+                            <strong>€ <?= number_format($product->getPrice(), 2) ?></strong>
+                            <br />
+                            <a href="#" class="card-link">Aggiungi al carrello</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <h4 class="my-3">
+            Prodotti per i gatti
+        </h4>
+        <div class="row row-cols-3 g-3">
+            <?php foreach ($products_cat as $product) : ?>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <i class="<?= $product->getCategory()->icon ?>"></i>
+                                <?= $product->getName() ?>
+                                <small class="text-muted">
+                                    <?= $product->getTextClassname() ?>
+                                </small>
+                            </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                <?php if ($product instanceof ProductBed || $product instanceof ProductFood) : ?>
+                                    <span><?= $product->getFullWeight() ?></span>
+                                <?php endif; ?>
+
+                                <?php if ($product instanceof ProductBed || $product instanceof ProductToy) : ?>
+                                    <span><?= $product->material ?></span>
+                                <?php endif; ?>
+
+                                <?php if ($product instanceof ProductFood) : ?>
+                                    <span><?= $product->type ?></span>
+                                <?php endif; ?>
+                            </h6>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, quasi.
+                            </p>
+                            <strong>€ <?= number_format($product->getPrice(), 2) ?></strong>
+                            <br />
+                            <a href="#" class="card-link">Aggiungi al carrello</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </body>
 
 </html>
